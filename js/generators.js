839b86df3f3839a2cd19793ccfa550038363b411
inv <isnave@gmail.com>
2016-11-15 20:06:08 +0100

    iterators and generators

---
+++ js/generators.js
@@ -0,0 +1,55 @@
+var obj = {
+    names: ["israel", "pepe", "manolo"],
+    [Symbol.iterator]: function*() {
+        yield* this.names;
+    }
+};
+
+for (let name of obj) {
+    console.log( name );
+}
+
+var myIterable = {};
+myIterable[Symbol.iterator] = function*() {
+    yield 1;
+    yield 2;
+    yield 3;
+    yield "fin";
+};
+
+for (let value of myIterable) {
+    console.log( value );
+}
+
+//aplanar el codigo
+var _ = {
+    flatten: (...args)=> {
+        return {
+            [Symbol.iterator]: function*() {
+                for (let i = 0; i < args.length; i++) {
+                    yield* args[i];
+                }
+            }
+        }
+    },
+    where: (iterable, conditionFunc)=> {
+        return {
+            [Symbol.iterator]: function*() {
+                for (let item of iterable) {
+                    if (conditionFunc( item )) {
+                        yield item;
+                    }
+                }
+            }
+        }
+    }
+}
+
+for (var res of _.flatten( ["1", "2"], ["3", "4"], ["5", "6", "7"] )) {
+    console.log( res );
+}
+var names = ["israel", "pepe", "manolo"];
+var hasM = _.where( names, name=>name.includes( "i" ) );
+for (var r of hasM) {
+    console.log( r );
+}
--- js/iterators.js
+++ js/iterators.js
@@ -19,21 +19,3 @@ var obj = {
 for (let name of obj) {
     console.log( name );
 }
-
-console.log( "otro iterador" );
-// otro iterador
-function makeIterator(array) {
-    var nextIndex = 0;
-    return {
-        next: function () {
-            return nextIndex < array.length ?
-            {value: array[nextIndex++], done: false} :
-            {done: true};
-        }
-    }
-}
-
-var it = makeIterator( ['yo', 'ya'] );
-console.log( it.next().value ); // 'yo'
-console.log( it.next().value ); // 'ya'
-console.log( it.next().done );  // true
